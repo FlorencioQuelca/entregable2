5976ba44496c7fd23a54970b07fe8fd89091a578
FlorencioQuelca <florens.lonwe@gmail.com>
2022-09-25 20:12:40 -0400

    avances

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 import './App.css'
+import WeaterCard from './components/WeaterCard'
 
 function App() {
-  const [count, setCount] = useState(0)
-   const APIKEY='b54a9d77500b05f1bebcb118a5eef36e'
-   const url='https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+
+  const [coords , setCoords] = useState()
+  const [weater , setWeater] = useState()
+  
+  useEffect(() => {
+    const success=(pos)=>{
+      const obj={
+          lat:pos.coords.latitude,
+          lon:pos.coords.longitude
+         }
+     setCoords(obj);
+    // console.log(obj)
+    }
+    navigator.geolocation.getCurrentPosition(success) 
+  }, [])
+  
+  console.log(coords)
+  useEffect(()=>{
+    
+    if(coords){
+      console.log(coords)
+      const APIKEY='b54a9d77500b05f1bebcb118a5eef36e'
+      const url=`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKEY}`
+
+      axios.get(url).then(res =>{
+    setWeater(res.data)
+      }).catch(err=>{
+        console.log(err)
+      })
+    }
+
+  },[coords]) 
+
+
   return (
     <div className="App">
-      
+         <WeaterCard  weater={weater}/>
     </div>
   )
 }
---
+++ src/components/WeaterCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WeaterCard = ({weater}) => {
+
+
+  return (
+    <article>
+           <h1>{weater?.name}</h1>
+    </article>
+  )
+
+}
+
+export default WeaterCard
